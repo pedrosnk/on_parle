182d2767ea818023e2ced92fc56515d9f81977dd
Pedro Medeiros <pedrosnk@gmail.com>
2016-05-01 15:57:38 -0300

    fix test case

--- test/on_parle_test.exs
+++ test/on_parle_test.exs
@@ -5,7 +5,7 @@ defmodule OnParleTest do
   test "Simple metaphone convention" do
     assert OnParle.metaphone("hello") == "HL"
     assert OnParle.metaphone("hypocrite") == "HPKRT"
-    assert OnParle.metaphone("WELL") == "WL"
+    assert OnParle.metaphone("well") == "WL"
     assert OnParle.metaphone("am") == "AM"
     assert OnParle.metaphone("say") == "S"
   end
